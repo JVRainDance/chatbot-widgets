c6db002e0b25e4b913aec42697a2d93fe2169fd3
JVRainDance <joshua@raindance.net.au>
2025-07-11 23:10:23 +0800

    Update chatbot_RAPID.js

--- chatbot_RAPID.js
+++ chatbot_RAPID.js
@@ -309,10 +309,8 @@
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
      xmlns="http://www.w3.org/2000/svg" stroke="currentColor" stroke-width="2">
   <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
-</svg> stroke="currentColor" stroke-width="2">
-                    <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-                </svg>
-            </button>
+</svg><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+                </svg></button>
             
             <div class="chat-widget-window" id="chat-widget-window">
                 <div class="chat-widget-header">
@@ -325,11 +323,9 @@
      xmlns="http://www.w3.org/2000/svg" stroke="currentColor" stroke-width="2">
   <line x1="18" y1="6" x2="6" y2="18"/>
   <line x1="6" y1="6" x2="18" y2="18"/>
-</svg> stroke="currentColor" stroke-width="2">
-                            <line x1="18" y1="6" x2="6" y2="18"></line>
+</svg><line x1="18" y1="6" x2="6" y2="18"></line>
                             <line x1="6" y1="6" x2="18" y2="18"></line>
-                        </svg>
-                    </button>
+                        </svg></button>
                 </div>
                 
                 <div class="chat-widget-messages" id="chat-widget-messages"></div>
