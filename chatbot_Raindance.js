b9dbacc54293093e9c360a4fe6c3bcbfa5300927
JVRainDance <joshua@raindance.net.au>
2025-07-11 22:22:48 +0800

    Update chatbot_Raindance.js

--- chatbot_Raindance.js
+++ chatbot_Raindance.js
@@ -306,9 +306,10 @@
     const chatHTML = `
         <div class="chat-widget-container">
             <button class="chat-widget-button" id="chat-widget-toggle">
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                    <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-                </svg>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+     xmlns="http://www.w3.org/2000/svg" stroke="currentColor" stroke-width="2">
+  <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
+</svg>
             </button>
             
             <div class="chat-widget-window" id="chat-widget-window">
@@ -318,10 +319,11 @@
                         <h3>${config.title}</h3>
                     </div>
                     <button class="chat-widget-close" id="chat-widget-close">
-                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                            <line x1="18" y1="6" x2="6" y2="18"></line>
-                            <line x1="6" y1="6" x2="18" y2="18"></line>
-                        </svg>
+                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none"
+     xmlns="http://www.w3.org/2000/svg" stroke="currentColor" stroke-width="2">
+  <line x1="18" y1="6" x2="6" y2="18"/>
+  <line x1="6" y1="6" x2="18" y2="18"/>
+</svg>
                     </button>
                 </div>
                 
