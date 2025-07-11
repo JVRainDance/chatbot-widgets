7131c876dbdc81a1fb4b5888f303a29c2e41baeb
JVRainDance <joshua@raindance.net.au>
2025-07-11 22:11:15 +0800

    Update chatbot_BIM.js

--- chatbot_BIM.js
+++ chatbot_BIM.js
@@ -324,9 +324,10 @@
                         <h3>${config.title}</h3>
                     </div>
                     <button class="chat-widget-close" id="chat-widget-close">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none"
      xmlns="http://www.w3.org/2000/svg" stroke="currentColor" stroke-width="2">
-  <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
+  <line x1="18" y1="6" x2="6" y2="18"/>
+  <line x1="6" y1="6" x2="18" y2="18"/>
 </svg>
                     </button>
                 </div>
